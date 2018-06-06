a071a9d0944769c1745a02c691796556211cf307
FERNANDO ELLERY <fernando.hle@gmail.com>
2018-06-06 15:16:59 -0300

    Criando operação de instanciação

--- src/main/java/com/fefeu/pedidos/PedidosApplication.java
+++ src/main/java/com/fefeu/pedidos/PedidosApplication.java
@@ -1,12 +1,32 @@
 package com.fefeu.pedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fefeu.pedidos.domain.Categoria;
+import com.fefeu.pedidos.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class PedidosApplication {
+public class PedidosApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 }
