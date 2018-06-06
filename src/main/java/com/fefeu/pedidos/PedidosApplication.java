52fa0020537441e53b61939ed7f4e34149864f0e
FERNANDO ELLERY <fernando.hle@gmail.com>
2018-06-06 16:05:29 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/fefeu/pedidos/PedidosApplication.java
+++ src/main/java/com/fefeu/pedidos/PedidosApplication.java
@@ -1,5 +1,6 @@
 package com.fefeu.pedidos;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fefeu.pedidos.domain.Categoria;
+import com.fefeu.pedidos.domain.Produto;
 import com.fefeu.pedidos.repositories.CategoriaRepository;
+import com.fefeu.pedidos.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PedidosApplication implements CommandLineRunner {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
@@ -25,8 +30,20 @@ public class PedidosApplication implements CommandLineRunner {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impressora", 800.00);
+		Produto p3 = new Produto(null,"Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 }
