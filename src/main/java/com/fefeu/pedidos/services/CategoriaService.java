4e5829138e35365edb1d99552a0aa46a78d6037c
FERNANDO ELLERY <fernando.hle@gmail.com>
2018-06-11 16:28:36 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/fefeu/pedidos/services/CategoriaService.java
+++ src/main/java/com/fefeu/pedidos/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.fefeu.pedidos.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fefeu.pedidos.domain.Categoria;
 import com.fefeu.pedidos.repositories.CategoriaRepository;
+import com.fefeu.pedidos.services.exceptions.DataIntegrityException;
 import com.fefeu.pedidos.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos");
+		}
+		
 	}
 
 }
