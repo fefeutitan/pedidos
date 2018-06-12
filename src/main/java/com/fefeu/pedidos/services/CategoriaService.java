dee068c16312a17cc6abeb07bd8d688616bc011f
FERNANDO ELLERY <fernando.hle@gmail.com>
2018-06-12 14:48:12 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/fefeu/pedidos/services/CategoriaService.java
+++ src/main/java/com/fefeu/pedidos/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.fefeu.pedidos.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos");
 		}
 		
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
